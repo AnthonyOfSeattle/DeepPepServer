856d679ee13a1d675c2fd300eb41c13cd37f650b
Anthony Valente <valenta4@uw.edu>
2021-03-04 10:43:25 -0800

    Update model config to accomodate preprocessing config change

--- deeppep/prediction/__init__.py
+++ deeppep/prediction/__init__.py
@@ -1,12 +1,15 @@
 from .manager import PredictionManager
 
 PREBUILT_CONFIG = {
-    "__default__": {"max_vocab_dim" : 26,
-                    "seq_len"       : 75,
-                    "one_hot"       : False,
-                    "config_path"   : "",
-                    "weight_path"   : ""},
-
-    "phospho_rt" : {"config_path" : "deeppep/prediction/models/phospho_rt_config.json",
-                    "weight_path" : "deeppep/prediction/models/phospho_rt_weights.h5"},
+    "human_phosphopedia_rt" : {
+        "pre_config" : {"vocab" : {"S[80]": 24, 
+                                   "T[80]": 25, 
+                                   "Y[80]": 26},
+                        "max_vocab_dim" : 26},
+        "config_path" : "deeppep/prediction/models/phospho_rt_config.json",
+        "weight_path" : "deeppep/prediction/models/phospho_rt_weights.h5"
+        },
 }
+
+PREBUILT_CONFIG["aliases"] = {"phospho_rt" : PREBUILT_CONFIG["human_phosphopedia_rt"]}
+
