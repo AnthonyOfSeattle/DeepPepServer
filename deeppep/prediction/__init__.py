a68fbe636b36874ba0de3543a1ca3d6c4384e6e4
Anthony Valente <valenta4@uw.edu>
2021-03-12 15:06:35 -0800

    Add config for new models

--- deeppep/prediction/__init__.py
+++ deeppep/prediction/__init__.py
@@ -1,15 +1,23 @@
 from .manager import PredictionManager
 
 PREBUILT_CONFIG = {
+    "phospho_rt_gru_512" : {
+        "pre_config" : {"vocab" : {"S[80]": 24,
+                                   "T[80]": 25,
+                                   "Y[80]": 26},
+                        "max_vocab_dim" : 26},
+        "config_path" : "deeppep/prediction/models/phospho_rt_gru_512.config.json",
+        "weight_path" : "deeppep/prediction/models/phospho_rt_gru_512.weights.h5"
+        },
     "human_phosphopedia_rt" : {
         "pre_config" : {"vocab" : {"S[80]": 24, 
                                    "T[80]": 25, 
                                    "Y[80]": 26},
                         "max_vocab_dim" : 26},
-        "config_path" : "deeppep/prediction/models/phospho_rt_config.json",
-        "weight_path" : "deeppep/prediction/models/phospho_rt_weights.h5"
+        "config_path" : "deeppep/prediction/models/human_phosphopedia_rt.config.json",
+        "weight_path" : "deeppep/prediction/models/human_phosphopedia_rt.weights.h5"
         },
 }
 
-PREBUILT_CONFIG["aliases"] = {"phospho_rt" : PREBUILT_CONFIG["human_phosphopedia_rt"]}
+PREBUILT_CONFIG["phospho_rt"] = PREBUILT_CONFIG["phospho_rt_gru_512"]
 
